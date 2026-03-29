56669e2daf8fc7937af4bea5f477f78f92383583
kushagra9926 <joshikushagra899@gmail.com>
2026-03-29 15:37:21 +0530

    Add initial implementation of assignment questions in Assignment_Loops.cpp

---
+++ Assignment_Loops.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+// Question 1: WAP to find the Factorial of a number entered by the user.
+// Question 2: WAP to print the multiplication table of a number, entered by the user.
+// Question 3: WAP to input a number and check whether the number is an Armstrong number or not.(a positive integer equal to the sum of its own digits, each raised to the power of the total number of digits.)
+// Question 4: For a positive N, WAP that prints all the prime numbers from 2 to N. (Assume N >= 2)
+// Question 5: For a positive N, WAP that prints the first N Fibonacci numbers.(series where each number is a sum of previous 2 numbers in the series.)
+    return 0;
+}
