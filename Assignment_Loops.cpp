ebb29a5b4b4e223841b4d4a6d0f04ebfa40cf2c8
kushagra9926 <joshikushagra899@gmail.com>
2026-03-29 23:34:22 +0530

    Enhance Assignment_Loops.cpp with Armstrong number check and prime number generation

--- Assignment_Loops.cpp
+++ Assignment_Loops.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
@@ -24,10 +25,58 @@ int main(){
             cout << n2 * i << endl;
         }
 
-// Question 3: WAP to input a number and check whether the number is an Armstrong number or not.(a positive integer equal to the sum of its own digits, each raised to the power of the total number of digits.)
+// Question 3: WAP to input a number and check whether the number is an Armstrong number or not.(Solve using three digit number.)
     // Solution: 
+        int n3;
+        cout << "Enter a number: ";
+        cin >> n3;
+        int sum = 0;
+        while(n3 > 0){
+            int digit = n3 % 10;
+            sum += digit * digit * digit;
+            n3 = n3 / 10; 
+        }
+        if(sum == n3){
+            cout << "Number is Armstrong" <<endl; 
+        }else{
+            cout << "Number is not Armstrong" << endl; 
+        }
         
 // Question 4: For a positive N, WAP that prints all the prime numbers from 2 to N. (Assume N >= 2)
+    // Solution: 
+        int n4;
+        cout << "Enter a number: ";
+        cin >> n4;
+        cout << "Prime Numbers are: "<< endl; 
+        for(int i = 2; i <= n4; i++){
+            int curr = i;
+            bool isPrime = true;
+
+            for(int j = 2; j <= sqrt(i); j++){
+                if(curr % j == 0){
+                    isPrime = false;
+                }
+            }
+            if(isPrime){
+                cout << curr << endl; 
+            }
+        }
 // Question 5: For a positive N, WAP that prints the first N Fibonacci numbers.(series where each number is a sum of previous 2 numbers in the series.)
+    // Solution: 
+        int n5;
+        cout << "Enter a number: ";
+        cin >> n5;
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        cout << a << " ";
+        cout << b << " ";
+        for(int i = 2; i < n5; i++){
+            c = a + b;
+            a = b;
+            b = c;
+            cout << c << " ";
+        }
+
     return 0;
 }
