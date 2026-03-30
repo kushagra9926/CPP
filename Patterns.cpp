170129ab7b77ea6c8aab45ff53257ef9d406210a
kushagra9926 <joshikushagra899@gmail.com>
2026-03-30 22:30:51 +0530

    Add solution for printing Character Pyramid pattern in Patterns.cpp

--- Patterns.cpp
+++ Patterns.cpp
@@ -70,7 +70,21 @@ int main(){
             }
             cout << endl;
         }
+// Print Character Pyramid pattern  A
+//                                  B C
+//                                  D E F
+//                                  G H I J
 
+    // Solution: 
+        int n5 = 4;
+        char ch = 'A';
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j <= i; j++){
+                cout << ch << " ";
+                ch++;
+            }
+            cout << endl;
+        }
 
         
     return 0;
