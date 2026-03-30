06633bf204f5bc850937633c4d7540a314a126f3
kushagra9926 <joshikushagra899@gmail.com>
2026-03-30 15:19:29 +0530

    Add initial structure for nested loop pattern printing in Patterns.cpp

---
+++ Patterns.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+// Nested Loop: Loop inside a loop.
+
+// Question: print  1 1 1 1 using loop.
+//                  2 2 2 2
+//                  3 3 3 3
+//                  4 4 4 4
+
+    // Solution: 
+        // 1) Outer loop: no of Rows
+        // 2) Inner loop: no of Columns/ each rows
+        // 3) Work in inner loop (each row) i print
+
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; )
+        }
+
+    return 0;
+}
