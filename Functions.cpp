a41e49fdadd5bc0ae294ac0e59e074383876932d
kushagra9926 <joshikushagra899@gmail.com>
2026-04-02 22:35:41 +0530

    Add initial implementation of Functions.cpp with sayHello function

---
+++ Functions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// Functions: Block of code which runs when it is called.
+        /* 
+        returnType fName(){
+        // do some work
+        return someValue;  // optional 
+        }
+        
+        fName();  //function call
+        */
+
+void sayHello(){
+    cout << "Hello :) \n";
+}
+
+int main(){
+    sayHello();  //function call
+    
+    return 0;
+}
