fdcc16af25236f42222546ba3f059ed13c6315e3
kushagra9926 <joshikushagra899@gmail.com>
2026-04-03 09:18:32 +0530

    Refactor Functions.cpp to include sum and diff functions with their respective calls in main

--- Functions.cpp
+++ Functions.cpp
@@ -20,8 +20,20 @@ void sayHello(){
     cout << "Hello :) \n";
 }
 
+int sum(int a , int b){
+    int sum = a + b;
+    return sum;
+}
+int diff(int a , int b){
+    int diff = a - b;
+    return diff;
+}
+
 int main(){
     sayHello();  //function call
-
+    int s = sum(2,4);   // 2,4 are arguments.
+    int d = diff(5,3);
+    cout << "sum = " << s << endl;
+    cout << "diff = " << d << endl;
     return 0;
 }
