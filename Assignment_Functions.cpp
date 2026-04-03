37992f8a422b9055f19810c193597b9680a9a828
kushagra9926 <joshikushagra899@gmail.com>
2026-04-03 22:44:02 +0530

    Refactor functions for clarity and add implementations for output, max_num, and next_char

--- Assignment_Functions.cpp
+++ Assignment_Functions.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 // Question 1: Write a function to check if a number is a palindrome in C++. (121 is a palindrome, 321 is not)
@@ -16,7 +17,6 @@ using namespace std;
             }else{
                 return false;
             }
-            
         }
 // Question 2: Write a function to calculate the sum of digits of a number.
     // Solution:
@@ -32,10 +32,30 @@ using namespace std;
         }
 // Question 3: Write a function which takes 2 numbers as parameters(a & b) and outputs: a^2 + b^2 + 2*ab.
     // Solution:
+        int output(int a, int b){
+            return pow(a, 2) + pow(b, 2) + 2 * a * b;
+        }
 // Question 4: Write a function that prints the largest of 3 numbers.
     // Solution: 
+        int max_num(int a, int b, int c){
+            if(a >= b && a >= c){
+                return a;
+            }
+            else if(b >= c){
+                return b;
+            }else{
+                return c;
+            }
+        }
 // Question 5: Write a function that accepts a character (ch) as parameters & returns the character that occurs after ch in the English alphabet. Eg: input = ‘c’, return value = ‘d’. Note: for ch = ‘z’, return ‘a’.
     // Solution:
+        char next_char(char a){
+            if(a == 'Z'){
+                return 'A';
+            }else{
+                return a + 1;
+            }  
+        }
 
 int main(){
     // 1) 
@@ -44,5 +64,14 @@ int main(){
 
     // 2)
         cout << sum_digit(12345) << endl;
+    
+    // 3)
+        cout << output(2, 3) << endl;
+
+    // 4)
+        cout << max_num(5, 6, 2) << endl;
+
+    // 5)
+        cout << next_char('Z') << endl;
     return 0;
 }
