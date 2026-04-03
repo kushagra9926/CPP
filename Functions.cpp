1f6c1d7155876a5d278969af915abb8dcaa9ba34
kushagra9926 <joshikushagra899@gmail.com>
2026-04-03 09:44:56 +0530

    Add default parameter to sum function and implement product and isEven functions with respective calls in main

--- Functions.cpp
+++ Functions.cpp
@@ -20,13 +20,24 @@ void sayHello(){
     cout << "Hello :) \n";
 }
 
-int sum(int a , int b){
+int sum(int a , int b = 1){  // Here we gave deffault value to second parameter when no value is assigned it'll take this value. Default value cannot be given to first parameter.
     int sum = a + b;
     return sum;
 }
 int diff(int a , int b){
     int diff = a - b;
     return diff;
+}
+int product(int a, int b){
+    int product = a * b;
+    return product;
+}
+bool isEven(int a){
+    if(a % 2 == 0){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 int main(){
@@ -35,5 +46,15 @@ int main(){
     int d = diff(5,3);
     cout << "sum = " << s << endl;
     cout << "diff = " << d << endl;
+
+// Question: WAF to find the product of 2 numbers a & b.
+    // Solution:
+        int p = product(2, 5);
+        cout << "product = " << p << endl;
+
+// Question: WAF to print if a number is odd or even.
+    // Solution: 
+        cout << isEven(20) << endl;
+
     return 0;
 }
