68f0cd6199639015e5388ffec52761b178d11823
kushagra9926 <joshikushagra899@gmail.com>
2026-04-06 23:57:03 +0530

    Add decToBin function to convert decimal numbers to binary and update main for demonstration

--- Assignment_Binary_Number_System.cpp
+++ Assignment_Binary_Number_System.cpp
@@ -22,10 +22,38 @@ using namespace std;
             cout << decNum << endl;
         }
         
+// Question 2: Convert the following decimal numbers in to binary forms: 
+    // ●25
+    // ●49
+    // ●31
+    // ●88
+
+    // Solution:
+        void decToBin(int decNum){
+            int n = decNum;
+            int pow = 1;
+            int binNum = 0;
+
+            while(n > 0){
+                int rem = n % 2;
+                binNum += rem * pow;
+                n = n / 2;
+                pow = pow * 10;
+            }
+            cout << "binary of " << decNum << " = " << binNum << endl;
+        }
+
+
         int main(){
             binToDec(111111);
             binToDec(10110); 
             binToDec(10011); 
             binToDec(110010); 
+
+            decToBin(25);
+            decToBin(49);
+            decToBin(31);
+            decToBin(88);
+
             return 0;
         }
