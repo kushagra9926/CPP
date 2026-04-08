29c353555692b59ef119d664da5f8291fc9c3a6d
kushagra9926 <joshikushagra899@gmail.com>
2026-04-08 12:15:30 +0530

    Refactor comments in Introduction_to_Pointers.cpp for clarity and consistency

--- Introduction_to_Pointers.cpp
+++ Introduction_to_Pointers.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+// Hexadecimal: These number become address of memory location. Hexadecimal numbers have base 16 [0 to 9, a(10), b(11), c(12), d(13), e(14), f(15)].
+
+// Address of Operator: '&' gives address of a variable.
+    /* 
+     For ex: 
+        int a = 10;
+        cout << &a;   // output: 0x61ff0c
+    */
 int main(){
     
     return 0;
