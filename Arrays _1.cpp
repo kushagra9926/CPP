93e7960ce70bd41c98ecaccd8fca065b293c8705
kushagra9926 <joshikushagra899@gmail.com>
2026-04-19 15:23:39 +0530

    Add comments to explain arrays and their creation in main function

--- Arrays _1.cpp
+++ Arrays _1.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+// Arrays: Linear collection of same type of elements that are stored together in contiguous memory spaces.
+
+// Creating an Array: 
+    /*
+    int marks[50];
+    int marks[50] = {1, 2, 3};
+    int marks[] = {1, 2, 3};
+
+    Memory is statically allocated (at compile time).
     
+    */
+int main(){
+
     return 0;
 }
