366ada36733fd560a1fab26b42247172397618a3
kushagra9926 <joshikushagra899@gmail.com>
2026-04-26 13:47:26 +0530

    Add detailed comments for subarrays and subsequences in Array_two.cpp

--- Array_two.cpp
+++ Array_two.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main(){
+
+// Print Subarrays: Subarray is continuous part of an array.
+    /*
+    arr[] = {1, 2, 3, 4, 5}
+
+    subarrays are: 
+        1, 12, 123, 1234, 12345
+        2, 23, 234, 2345
+        3, 34, 345
+        4, 45
+        5   
+
     
+    subsequence (non-continuous): 
+        1 3 5 // this is subsquence but not subarray. Also in subsequence if one comes before in array it should come before in subsequence also.
+
+
+    */
     return 0;
 }
