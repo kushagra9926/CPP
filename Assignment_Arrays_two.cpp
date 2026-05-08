4bb4b6a190b11a70ab994321f9bb367227c5cdcf
kushagra9926 <joshikushagra899@gmail.com>
2026-05-08 15:52:06 +0530

    Add problem statement and examples for rotated sorted array search in Assignment_Arrays_two.cpp

--- Assignment_Arrays_two.cpp
+++ Assignment_Arrays_two.cpp
@@ -11,6 +11,24 @@ Input: nums = [1,1,1,3,3,4,3,2,4,2]
 Output: true
 */
 
+/*
+Question 2. There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Examples:
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+*/
 int main(){
 
     return 0;
