2c2576d4063e831eecc518d13527cd5d3776f32b
kushagra9926 <joshikushagra899@gmail.com>
2026-05-08 15:53:38 +0530

    Add problem statement and examples for largest product subarray in Assignment_Arrays_two.cpp

--- Assignment_Arrays_two.cpp
+++ Assignment_Arrays_two.cpp
@@ -20,14 +20,30 @@ Given the array nums after the possible rotation and an integer target, return t
 
 You must write an algorithm with O(log n) runtime complexity.
 
- 
-
 Examples:
 Input: nums = [4,5,6,7,0,1,2], target = 0
 Output: 4
 
 Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
+*/
+
+/*
+Question 3. Given an integer array nums, find a subarray that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+Note that the product of an array with a single element is the value of that element.
+
+Examples:
+
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
 */
 int main(){
 
