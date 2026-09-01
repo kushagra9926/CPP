792406c618c9c57dc68f4d7d1075a54faef507d6
kushagra9926 <joshikushagra899@gmail.com>
2026-09-01 22:59:13 +0530

    Refactor Hashing.cpp to improve code clarity and add missing comments for better understanding of hashing techniques

--- Notes/Hashing.cpp
+++ Notes/Hashing.cpp
@@ -23,9 +23,14 @@ When Unordered_map is used:
     Time complexity: 
     in average and best case = O(1)
 
-    but in worst case = O(N)  Here worst case happen due to internal collisons
+    but in worst case = O(N)  Here worst case happen due to internal collisons (when all keys end up at same value)
 
 ## Most of the time use unordered_map but if it gives time limit exceeded then switch to map.
+
+    Hashing ----> * Division method
+                  * Folding Method
+                  * Mid Square Method
+ 
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -34,94 +39,94 @@ int main(){
     // If we have to find the count of the element in an array.
 
     // // Number Hashing
-    // int n1;
-    // cin >> n1;
-    // int arr[n1];
-    // for(int i = 0; i < n1; i++){
-    //     cin >> arr[i];
-    // }
+    int n1;
+    cin >> n1;
+    int arr[n1];
+    for(int i = 0; i < n1; i++){
+        cin >> arr[i];
+    }
 
-    // // precompute
-    // int hash1[13] = {0};
-    // for(int i = 0; i < n1; i++){
-    //     hash1[arr[i]] += 1;
-    // }
+    // precompute
+    int hash1[13] = {0};
+    for(int i = 0; i < n1; i++){
+        hash1[arr[i]] += 1;
+    }
 
-    // int q1;
-    // cin >> q1;
-    // while(q1--){
-    //     int num;
-    //     cin >> num;
-    //     // fetch
-    //     cout << hash1[num] << endl;
-    // }
+    int q1;
+    cin >> q1;
+    while(q1--){
+        int num;
+        cin >> num;
+        // fetch
+        cout << hash1[num] << endl;
+    }
 
     // Character Hashing
-    // string s1;
-    // cin >> s1;
+    string s1;
+    cin >> s1;
     
-    // // pre compute
-    // int hash2[26] = {0};
-    // for(int i = 0; i < s1.size(); i++){
-    //     hash2[s1[i] - 'a']++;
-    // }
+    // pre compute
+    int hash2[26] = {0};
+    for(int i = 0; i < s1.size(); i++){
+        hash2[s1[i] - 'a']++;
+    }
 
-    // int q2;
-    // cin >> q2;
-    // while(q2--){
-    //     char c;
-    //     cin >> c;
-    //     // fetch
-    //     cout  << hash2[c - 'a']<< endl;
-    // }
+    int q2;
+    cin >> q2;
+    while(q2--){
+        char c;
+        cin >> c;
+        // fetch
+        cout  << hash2[c - 'a']<< endl;
+    }
 
     // // In the previous example only small alphabets are asked, but if all characters are to be used then
-    // string s2;
-    // cin >> s2;
+    string s2;
+    cin >> s2;
     
-    // // pre compute
-    // int hash3[256] = {0};
-    // for(int i = 0; i < s2.size(); i++){
-    //     hash3[s2[i]]++;
-    // }
+    // pre compute
+    int hash3[256] = {0};
+    for(int i = 0; i < s2.size(); i++){
+        hash3[s2[i]]++;
+    }
 
-    // int q3;
-    // cin >> q3;
-    // while(q3--){
-    //     char c;
-    //     cin >> c;
-    //     // fetch
-    //     cout  << hash3[c]<< endl;
-    // }
+    int q3;
+    cin >> q3;
+    while(q3--){
+        char c;
+        cin >> c;
+        // fetch
+        cout  << hash3[c]<< endl;
+    }
 
     // Using Map 
-    // int n2;
-    // cin >> n2;
-    // int arr[n2];
+    int n2;
+    cin >> n2;
+    int arr[n2];
 
-    // for(int i = 0; i < n2; i++){
-    //     cin >> arr[i];
-    // }
+    for(int i = 0; i < n2; i++){
+        cin >> arr[i];
+    }
 
-    // // pre compute
-    // map<int, int> mpp2;
-    // for(int i = 0; i < n2; i++){
-    //     mpp[arr[i]]++;
-    // }
+    // pre compute
+    map<int, int> mpp2;
+    for(int i = 0; i < n2; i++){
+        mpp2[arr[i]]++;
+    }
     
-    // // // iterate in the map
-    // // for(auto it : mpp2){
-    // //     cout << it.first << "->" << it.second << endl;
-    // // }
-
-    // int q3;
-    // cin >> q3;
-    // while(q3--){
-    //     int num;
-    //     cin >> num;
-    //     // fetch
-    //     cout << mpp2[num]<< endl;
+    // // iterate in the map
+    // for(auto it : mpp2){
+    //     cout << it.first << "->" << it.second << endl;
     // }
+
+    int q3;
+    cin >> q3;
+    while(q3--){
+        int num;
+        cin >> num;
+        // fetch
+        cout << mpp2[num]<< endl;
+    }
 
     // Unordered Map
     int n3;
